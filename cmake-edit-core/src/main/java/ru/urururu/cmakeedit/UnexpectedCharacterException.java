206472bb78e1086fb7f8b62c1c5c28576e9e5f02
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2016-07-11 11:47:40 +0300

    Removed source reference from UnexpectedCharacterException as it gives no valuable information.

--- cmake-edit-core/src/main/java/ru/urururu/cmakeedit/UnexpectedCharacterException.java
+++ cmake-edit-core/src/main/java/ru/urururu/cmakeedit/UnexpectedCharacterException.java
@@ -1,12 +1,10 @@
 package ru.urururu.cmakeedit;
 
-import com.codahale.metrics.MetricRegistry;
-
 /**
  * Created by okutane on 06/07/16.
  */
 public class UnexpectedCharacterException extends ParseException {
     public UnexpectedCharacterException(ParseContext ctx) {
-        super(ctx, "Unexpected character '" + ctx.peek() + "' at " + ctx.position().getOffset());
+        super(ctx, "Unexpected character '" + ctx.peek() + '\'');
     }
 }

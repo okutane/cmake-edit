3e4afdef6d40824cdfdc12428193a3599f72b8c0
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2019-09-23 19:43:39 +0300

    Using external process as lsp server

--- vscode-cmake/src/extension.ts
+++ vscode-cmake/src/extension.ts
@@ -1,27 +1,52 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as path from 'path';
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient';
+
+let client: LanguageClient;
+
 export function activate(context: vscode.ExtensionContext) {
+	// The server is implemented in node
+	let serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
+	// The debug options for the server
+	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-cmake" is now active!');
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	let serverOptions: ServerOptions = {
+		run: { command: "java", args: ["-jar a.jar"] },
+		debug: {
+			command: "java", args: ["-jar", "b.jar"]
+		}
+	};
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'cmake',
+		'CMake',
+		serverOptions,
+		clientOptions
+	);
 
-	context.subscriptions.push(disposable);
+	// Start the client. This will also launch the server
+	client.start();
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }

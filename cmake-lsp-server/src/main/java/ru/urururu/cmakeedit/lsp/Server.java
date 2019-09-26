224ba744c13e6a81ef669ea5585e191cdafebaf8
Dmitry Matveev <dmitriy.g.matveev@gmail.com>
2019-09-26 12:24:07 +0300

    fixed capabilities

--- cmake-lsp-server/src/main/java/ru/urururu/cmakeedit/lsp/Server.java
+++ cmake-lsp-server/src/main/java/ru/urururu/cmakeedit/lsp/Server.java
@@ -1,11 +1,127 @@
 package ru.urururu.cmakeedit.lsp;
 
+import org.eclipse.lsp4j.CompletionOptions;
+import org.eclipse.lsp4j.DidChangeConfigurationParams;
+import org.eclipse.lsp4j.DidChangeTextDocumentParams;
+import org.eclipse.lsp4j.DidChangeWatchedFilesParams;
+import org.eclipse.lsp4j.DidCloseTextDocumentParams;
+import org.eclipse.lsp4j.DidOpenTextDocumentParams;
+import org.eclipse.lsp4j.DidSaveTextDocumentParams;
+import org.eclipse.lsp4j.InitializeParams;
+import org.eclipse.lsp4j.InitializeResult;
+import org.eclipse.lsp4j.MessageParams;
+import org.eclipse.lsp4j.MessageType;
+import org.eclipse.lsp4j.ServerCapabilities;
+import org.eclipse.lsp4j.TextDocumentSyncKind;
+import org.eclipse.lsp4j.TextDocumentSyncOptions;
+import org.eclipse.lsp4j.jsonrpc.Launcher;
+import org.eclipse.lsp4j.launch.LSPLauncher;
+import org.eclipse.lsp4j.services.LanguageClient;
+import org.eclipse.lsp4j.services.LanguageClientAware;
+import org.eclipse.lsp4j.services.LanguageServer;
+import org.eclipse.lsp4j.services.TextDocumentService;
+import org.eclipse.lsp4j.services.WorkspaceService;
+
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+
 public class Server {
     public static void main(String... args) {
-        LanguageServer server = new LanguageServer {} ;
-Launcher<LanguageClient> launcher = 
-    LSPLauncher.createServerLauncher(server,
-                                     inputstream, 
-                                     outputstream);
+        TextDocumentService textDocumentService = new TextDocumentService() {
+            @Override
+            public void didOpen(DidOpenTextDocumentParams didOpenTextDocumentParams) {
+                throw new IllegalStateException(didOpenTextDocumentParams.toString());
+            }
+
+            @Override
+            public void didChange(DidChangeTextDocumentParams didChangeTextDocumentParams) {
+
+            }
+
+            @Override
+            public void didClose(DidCloseTextDocumentParams didCloseTextDocumentParams) {
+
+            }
+
+            @Override
+            public void didSave(DidSaveTextDocumentParams didSaveTextDocumentParams) {
+
+            }
+        };
+
+        WorkspaceService workspaceService = new WorkspaceService() {
+            @Override
+            public void didChangeConfiguration(DidChangeConfigurationParams didChangeConfigurationParams) {
+
+            }
+
+            @Override
+            public void didChangeWatchedFiles(DidChangeWatchedFilesParams didChangeWatchedFilesParams) {
+
+            }
+        };
+
+        LanguageServer server = new CMakeLanguageServer(textDocumentService, workspaceService);
+
+        Launcher<LanguageClient> launcher =
+                LSPLauncher.createServerLauncher(server,
+                        System.in,
+                        System.out);
+
+        launcher.startListening();
+    }
+
+    private static class CMakeLanguageServer implements LanguageServer, LanguageClientAware {
+        private final TextDocumentService textDocumentService;
+        private final WorkspaceService workspaceService;
+
+        public CMakeLanguageServer(TextDocumentService textDocumentService, WorkspaceService workspaceService) {
+            this.textDocumentService = textDocumentService;
+            this.workspaceService = workspaceService;
+        }
+
+        @Override
+        public CompletableFuture<InitializeResult> initialize(InitializeParams initializeParams) {
+            ServerCapabilities serverCapabilities = new ServerCapabilities();
+            serverCapabilities.setColorProvider(true);
+            serverCapabilities.setDocumentFormattingProvider(true);
+            serverCapabilities.setDocumentHighlightProvider(true);
+            TextDocumentSyncOptions textDocumentSync = new TextDocumentSyncOptions();
+            textDocumentSync.setOpenClose(true);
+            textDocumentSync.setChange(TextDocumentSyncKind.Full);
+            serverCapabilities.setTextDocumentSync(textDocumentSync);
+
+            serverCapabilities.setCompletionProvider(new CompletionOptions(true, null));
+
+            CompletableFuture<InitializeResult> result = new CompletableFuture<>();
+            result.complete(new InitializeResult(serverCapabilities));
+
+            return result;
+        }
+
+        @Override
+        public CompletableFuture<Object> shutdown() {
+            return null;
+        }
+
+        @Override
+        public void exit() {
+
+        }
+
+        @Override
+        public TextDocumentService getTextDocumentService() {
+            return textDocumentService;
+        }
+
+        @Override
+        public WorkspaceService getWorkspaceService() {
+            return workspaceService;
+        }
+
+        @Override
+        public void connect(LanguageClient languageClient) {
+            languageClient.showMessage(new MessageParams(MessageType.Info, "connected"));
+        }
     }
 }
--- vscode-cmake/src/extension.ts
+++ vscode-cmake/src/extension.ts
@@ -11,25 +11,19 @@ import {
 let client: LanguageClient;
 
 export function activate(context: vscode.ExtensionContext) {
-	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
-
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
+	let java = "/Library/Java/JavaVirtualMachines/jdk1.8.0_172.jdk/Contents/Home/bin/java";
+	let debugAgent = "-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005";
 	let serverOptions: ServerOptions = {
-		run: { command: "java", args: ["-jar a.jar"] },
+		run: { command: java, args: ["-jar a.jar"] },
 		debug: {
-			command: "java", args: ["-jar", "b.jar"]
+			command: java, args: ["-jar", "/Users/dmitry.matveyev/okutane/cmake-edit/cmake-lsp-server/target/cmake-lsp-server-1.0-SNAPSHOT-jar-with-dependencies.jar"]
 		}
 	};
 
 	// Options to control the language client
 	let clientOptions: LanguageClientOptions = {
 		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+		documentSelector: [{ scheme: 'file', language: 'cmake' }],
 		synchronize: {
 			// Notify the server about file changes to '.clientrc files contained in the workspace
 			fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc')
